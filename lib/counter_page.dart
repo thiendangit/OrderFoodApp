9dcfde57f4cd27d053d987b9f0cc1957a6b0bc01
thiendangit <thiendangit1102@gmail.com>
2021-08-16 00:42:21 +0700

    Couter state

---
+++ lib/counter_page.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'bloc/counter_bloc.dart';
+
+class CounterPage extends StatelessWidget {
+  static const String routeName = '/CounterPage';
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: BlocProvider<CounterCubit>(
+      create: (context) => CounterCubit(),
+      child: CounterView(),
+    ));
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
+
+/// {@template counter_view}
+/// A [StatelessWidget] which reacts to the provided
+/// [CounterCubit] state and notifies it in response to user input.
+/// {@endtemplate}
+class CounterView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+    return Scaffold(
+      appBar: AppBar(title: const Text('Counter')),
+      body: Center(
+        child: BlocBuilder<CounterCubit, int>(
+          builder: (context, state) {
+            return Text('$state', style: textTheme.headline2);
+          },
+        ),
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: <Widget>[
+          FloatingActionButton(
+            key: const Key('counterView_increment_floatingActionButton'),
+            child: const Icon(Icons.add),
+            onPressed: () => context.read<CounterCubit>().increment(),
+          ),
+          const SizedBox(height: 8),
+          FloatingActionButton(
+            key: const Key('counterView_decrement_floatingActionButton'),
+            child: const Icon(Icons.remove),
+            onPressed: () => context.read<CounterCubit>().decrement(),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,61 @@
 import 'dart:ui';
 
+import 'package:awesome_app/bloc/counter_bloc.dart';
 import 'package:awesome_app/categories_page.dart';
+import 'package:awesome_app/counter_page.dart';
 import 'package:awesome_app/detail_food_page.dart';
 import 'package:awesome_app/foods_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MaterialApp(
+    title: "Food App with Navigation",
+    initialRoute: '/',
+    routes: {
+      FoodsPage.routeName: (context) => FoodsPage(),
+      DetailFoodPage.routeName: (context) => DetailFoodPage(),
+      CounterPage.routeName: (context) => CounterPage(),
+    },
+    theme: ThemeData(
+        primarySwatch: Colors.green,
+        fontFamily: 'Itim',
+        textTheme: ThemeData.light().textTheme.copyWith(
+            body1: TextStyle(color: Color.fromRGBO(20, 52, 52, 1)),
+            body2: TextStyle(color: Color.fromRGBO(20, 52, 52, 1)),
+            title: TextStyle(
+                fontSize: 25, color: Colors.white, fontFamily: 'Rajdhani'))),
+    home: MyApp()));
 
 class MyApp extends StatelessWidget {
+  Stream<int> aStreamFunction() async* {
+    yield 2;
+    yield 5;
+    yield 7;
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return MaterialApp(
-      title: "Food App with Navigation",
-      initialRoute: '/',
-      routes: {
-        FoodsPage.routeName: (context) => FoodsPage(),
-        DetailFoodPage.routeName: (context) => DetailFoodPage(),
-      },
-      theme: ThemeData(
-          primarySwatch: Colors.green,
-          fontFamily: 'Itim',
-          textTheme: ThemeData.light().textTheme.copyWith(
-              body1: TextStyle(color: Color.fromRGBO(20, 52, 52, 1)),
-              body2: TextStyle(color: Color.fromRGBO(20, 52, 52, 1)),
-              title: TextStyle(
-                  fontSize: 25, color: Colors.white, fontFamily: 'Rajdhani'))),
-      home: Scaffold(
-        appBar: AppBar(title: Text('Food Categories')),
-        body: CategoriesPage(),
-      ),
-    );
+    final aStream = this.aStreamFunction();
+    aStream.forEach((element) {});
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Food Categories'),
+          actions: [
+            InkWell(
+              onTap: () {
+                Navigator.pushNamed(context, CounterPage.routeName);
+              },
+              child: Container(
+                padding: const EdgeInsets.all(10.0),
+                child: Icon(
+                  Icons.add_box_rounded,
+                ),
+              ),
+            )
+          ],
+        ),
+        body: CategoriesPage());
     throw UnimplementedError();
   }
 }
